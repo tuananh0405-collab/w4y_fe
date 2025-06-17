c28c09b58e768c533bb320f81da99920841b3d86
Quatn <nguyenminhquanvh@gmail.com>
2025-06-17 14:57:19 +0700

    feature/chat-room: Separated VITE_BASE_URL from VITE_SOCKET_BASE_URL (can be ignored in development env)

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 import { BASE_URL } from "./redux/constants";
 
-export const socket = io(BASE_URL, {
+export const socket = io(import.meta.env.VITE_SOCKET_BASE_URL ?? BASE_URL, {
   path: (import.meta.env.VITE_SOCKET_PATH ?? "") + "/socket.io",
   // transports: ['websocket', 'polling']
 });
